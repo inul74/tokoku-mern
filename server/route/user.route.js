ff91544270e26d859045c150643aaf712ff9d4ab
inul74 <zmusthofa74@gmail.com>
2024-11-18 04:12:08 +0700

    user update api

--- server/route/user.route.js
+++ server/route/user.route.js
@@ -6,6 +6,7 @@ import {
   loginController,
   logoutController,
   uploadAvatar,
+  updateUserDetails,
 } from "../controllers/user.controller.js";
 import auth from "../middleware/auth.js";
 import upload from "../middleware/multer.js";
@@ -17,5 +18,6 @@ userRouter.post("/verify-email", verifyEmailController);
 userRouter.post("/login", loginController);
 userRouter.get("/logout", auth, logoutController);
 userRouter.put("/upload-avatar", auth, upload.single("avatar"), uploadAvatar);
+userRouter.put("/update-user", auth, updateUserDetails);
 
 export default userRouter;
