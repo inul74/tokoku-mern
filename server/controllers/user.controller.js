0430cf0dfc16319b60906835c4709fb324024d08
inul74 <zmusthofa74@gmail.com>
2024-11-17 23:42:14 +0700

    user route: login & logout

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -3,6 +3,8 @@ import bcryptjs from "bcryptjs";
 import UserModel from "../models/user.model.js";
 import sendEmail from "../config/sendEmail.js";
 import verifyEmailTemplate from "../utils/verifyEmailTemplate.js";
+import generatedAccessToken from "../utils/generatedAccessToken.js";
+import generatedRefreshToken from "../utils/generatedRefreshToken.js";
 
 export async function registerUserController(request, response) {
   try {
@@ -111,6 +113,128 @@ export async function verifyEmailController(request, response) {
       message: error.message || error,
       error: true,
       success: true,
+    });
+  }
+}
+
+//login controller
+export async function loginController(request, response) {
+  try {
+    const { email, password } = request.body;
+
+    if (!email || !password) {
+      return response.status(400).json({
+        message: "provide email, password",
+        error: true,
+        success: false,
+      });
+    }
+
+    const user = await UserModel.findOne({ email });
+
+    if (!user) {
+      return response.status(400).json({
+        message: "User not register",
+        error: true,
+        success: false,
+      });
+    }
+
+    if (user.status !== "Active") {
+      return response.status(400).json({
+        message: "Contact to Admin",
+        error: true,
+        success: false,
+      });
+    }
+
+    const checkPassword = await bcryptjs.compare(password, user.password);
+
+    if (!checkPassword) {
+      return response.status(400).json({
+        message: "Check your password",
+        error: true,
+        success: false,
+      });
+    }
+
+    const accessToken = generatedAccessToken(user._id);
+    const refreshToken = await generatedRefreshToken(user._id);
+
+    const updateUser = await UserModel.findByIdAndUpdate(user?._id, {
+      last_login_date: new Date(),
+    });
+
+    if (!updateUser) {
+      return response.status(400).json({
+        message: "Login failed",
+        error: true,
+        success: false,
+      });
+    }
+
+    const cookiesOption = {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    };
+    response.cookie("accessToken", accessToken, cookiesOption);
+    response.cookie("refreshToken", refreshToken, cookiesOption);
+
+    return response.json({
+      message: "Login successfully",
+      error: false,
+      success: true,
+      data: {
+        accessToken,
+        refreshToken,
+      },
+    });
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
+    });
+  }
+}
+
+//logout controller
+export async function logoutController(request, response) {
+  try {
+    const userid = request.userId; //middleware
+
+    const cookiesOption = {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None",
+    };
+
+    response.clearCookie("accessToken", cookiesOption);
+    response.clearCookie("refreshToken", cookiesOption);
+
+    const removeRefreshToken = await UserModel.findByIdAndUpdate(userid, {
+      refresh_token: "",
+    });
+
+    if (!removeRefreshToken) {
+      return response.status(400).json({
+        message: "Logout failed",
+        error: true,
+        success: false,
+      });
+    }
+
+    return response.json({
+      message: "Logout successfully",
+      error: false,
+      success: true,
+    });
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message || error,
+      error: true,
+      success: false,
     });
   }
 }
--- server/route/user.route.js
+++ server/route/user.route.js
@@ -3,11 +3,16 @@ import { Router } from "express";
 import {
   registerUserController,
   verifyEmailController,
+  loginController,
+  logoutController,
 } from "../controllers/user.controller.js";
+import auth from "../middleware/auth.js";
 
 const userRouter = Router();
 
 userRouter.post("/register", registerUserController);
 userRouter.post("/verify-email", verifyEmailController);
+userRouter.post("/login", loginController);
+userRouter.get("/logout", auth, logoutController);
 
 export default userRouter;
